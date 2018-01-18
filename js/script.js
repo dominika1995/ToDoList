efdffa6ddb97256ad1171f939b3dcd16dd70533c
DominikaK <dominika.kochanska95@gmail.com>
2018-01-18 23:06:38 +0100

    function for new element in dom

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-   $('.list__item').on("click", function(){
+
+   $('.list__items').on("click","li", function(){
        if($(this).find('.list__items--text').css("text-decoration")[0]=="n"){
            $(this).find('.list__items--text').css("text-decoration","line-through");
        }
@@ -9,17 +10,19 @@ $(document).ready(function() {
        }
    });
    
-   $(".list__items--trash").on("click",function () {
-       $(this).parent(".list__item").remove();
+   $(".list__items").on("click",".list__items--trash",function (event) {
+       $(this).parent(".list__item").fadeOut(500,function () {
+           $(this).parent(".list__item").remove();
+       });
+       event.stopPropagation();
    });
 
     $("input:text").keypress(function(e) {
         if(e.which == 13) {
+            var text=$("input:text").val();
             $(".list__items").append(
-            '<li class="list__item">'+
-                '<span class="list__items--trash">'+
-                '<i class="fa fa-trash"></i>'+
-                '</span><span class="list__items--text">'+$("input:text").val()+'</span></li>')
+            '<li class="list__item"><span class="list__items--trash"><i class="fa fa-trash"></i>'+
+                '</span><span class="list__items--text">'+text+'</span></li>')
         }
     });
 
